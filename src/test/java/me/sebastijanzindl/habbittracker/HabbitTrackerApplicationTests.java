b1eca822756ff655c61a21589aebb25cdffb57ac
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-05 14:22:36 +0100

    initial commit

---
+++ src/test/java/me/sebastijanzindl/habbittracker/HabbitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sebastijanzindl.habbittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabbitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
